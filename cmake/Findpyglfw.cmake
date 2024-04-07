9ef5d6af217cb6f16dbe6be5269742443421ca24
alagyn <benkimbrough44@gmail.com>
2024-04-07 11:45:41 -0400

    move glad to shared lib in pyglfw

--- cmake/Findpyglfw.cmake
+++ cmake/Findpyglfw.cmake
@@ -16,7 +16,7 @@ file(GLOB pyglfw_LIBRARY "${pyglfw_GLOB}")
 
 find_path(pyglfw_INCLUDE
     GLFW/glfw3.h
-    PATHS ${pyglfw_ROOT}/py-glfw-redux-${pyglfw_VERSION}.inc
+    PATHS ${pyglfw_ROOT}/py-glfw-redux-${pyglfw_VERSION}.inc/glfw
     REQUIRED
 )
 
@@ -24,16 +24,42 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     pyglfw
     REQUIRED_VARS
-        pyglfw_LIBRARY
-        pyglfw_INCLUDE
+    pyglfw_LIBRARY
+    pyglfw_INCLUDE
     VERSION_VAR
-        pyglfw_VERSION
+    pyglfw_VERSION
 )
 
 if(pyglfw_FOUND)
     if(NOT TARGET pyglfw)
         add_library(pyglfw INTERFACE)
-        target_link_libraries(pyglfw INTERFACE ${pyglfw_LIBRARY})
-        target_include_directories(pyglfw INTERFACE ${pyglfw_INCLUDE})
+        target_link_libraries(pyglfw
+            INTERFACE
+            ${pyglfw_LIBRARY}
+        )
+        target_include_directories(pyglfw
+            INTERFACE
+            ${pyglfw_INCLUDE}
+        )
+
+        if(MSVC)
+            target_link_libraries(pyglfw
+                INTERFACE
+                opengl32.lib
+                gdi32.lib
+                shell32.lib
+            )
+        else()
+            find_package(OpenGL REQUIRED)
+            target_link_libraries(pyglfw
+                INTERFACE
+                ${OPENGL_LIBRARIES}
+            )
+        endif()
+
+        target_link_libraries(pyglfw
+            INTERFACE
+            ${Python3_LIBRARIES}
+        )
     endif()
 endif()
